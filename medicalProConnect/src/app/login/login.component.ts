db518e6ac44f4e12eb0556da1a0b62572ad3eed0
ashifur-rahman23 <ashifur.malia@gmail.com>
2024-08-23 10:57:27 +1000

    main branch fix

--- medicalProConnect/src/app/login/login.component.ts
+++ medicalProConnect/src/app/login/login.component.ts
@@ -15,6 +15,7 @@ export class LoginComponent {
   passwordInputType = 'password'
   submitted = false
   buttonType = ButtonType.SUBMIT
+  test = ''
 
   onEmailChange(email: string) {
     this.emailText = email
