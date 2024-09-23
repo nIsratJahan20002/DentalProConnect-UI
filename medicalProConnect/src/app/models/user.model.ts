49f0d4477695e68a29a442ad503888bd1f129504
nIsratJahan20002 <jahan.israt20002@gmail.com>
2024-09-23 20:13:06 +0600

    has been resolve problem for login and registration services

--- medicalProConnect/src/app/models/user.model.ts
+++ medicalProConnect/src/app/models/user.model.ts
@@ -6,26 +6,3 @@ export class UserDto {
   
 }
 
-// export class Person{
-//   id: number = 0;
-//   name: string = '';
-// }
-
-// export interface KeyValue{
-//   key: string;
-//   value: any;
-// }
-
-// let test1:KeyValue = {key: 1, value:'Israt'}
-
-// let personObj = new Person();
-// personObj.name = 'Israt';
-// personObj.id = 1;
-
-// let personObj2 = new Person();
-// personObj2.id=2;
-// personObj2.name = 'Puson';
-
-// if (personObj.id == personObj2.id) {
-
-// }
